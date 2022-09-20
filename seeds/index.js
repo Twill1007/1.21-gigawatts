079e8863557b6481425bfb9a9dc1baaa6b3f5270
Twill1007 <thomaswillford@gmail.com>
2022-09-19 21:22:41 -0700

    Yelp-Camp 9/19 922pm

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod numquam sed temporibus deleniti natus enim laborum tenetur. Itaque vel aut dolor quaerat ab sed ex minima! Maxime quas excepturi voluptate. Rerum unde ipsum accusantium est culpa ipsa temporibus atque voluptatem dolorem expedita, quo praesentium nostrum error, eligendi enim, soluta facere natus doloremque ducimus numquam quas vitae exercitationem nulla labore. Corrupti! Dolor consequuntur ipsam, ex molestiae iste eligendi corporis, facere tenetur, sit voluptate exercitationem soluta laudantium saepe magnam mollitia accusantium earum beatae consequatur atque fugiat. Nulla molestias aliquid voluptatem qui possimus?',
+            price
         })
         await camp.save();
     }
