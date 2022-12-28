6090785227edba58bda00b56ffb46e561cde052b
Twill1007 <thomaswillford@gmail.com>
2022-12-27 19:04:16 -0700

    YelpCamp

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,19 +1,17 @@
-const goodCampground = JSON.parse(campground);
-
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/streets-v10', // style URL
-    center: goodCampground.geometry.coordinates, // starting position [lng, lat]
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
 
 new mapboxgl.Marker()
-    .setLngLat(goodCampground.geometry.coordinates)
+    .setLngLat(campground.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
             .setHTML(
-                `<h4>${goodCampground.title}<h4><p>${goodCampground.location}</p>`)
+                `<h4>${campground.title}<h4><p>${campground.location}</p>`)
     )
     .addTo(map);
